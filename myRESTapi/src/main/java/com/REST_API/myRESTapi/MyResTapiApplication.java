b86f8f70ff984bb704f409fae0b7424f02464f4b
AbidHasnine <abidhasnine12@gmail.com>
2026-02-25 22:50:40 +0600

    REST_API practice and concept clear

---
+++ myRESTapi/src/main/java/com/REST_API/myRESTapi/MyResTapiApplication.java
@@ -0,0 +1,13 @@
+package com.REST_API.myRESTapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyResTapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyResTapiApplication.class, args);
+	}
+
+}
---
+++ myRESTapi/src/test/java/com/REST_API/myRESTapi/MyResTapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.REST_API.myRESTapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyResTapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
